ab9fb6211dd79b8b3f379a431b6aad0ece39d7de
andreeezinho <andreeesapucaia@gmail.com>
2025-03-02 20:11:54 -0300

    feat: add products in dashboard

--- src/Resources/Views/dashboard/index.php
+++ src/Resources/Views/dashboard/index.php
@@ -4,11 +4,15 @@
 
 <div class="container">
     <div class="row">
-        <div class="text-muted border-bottom d-flex">
-            <p class="m-0 p-0 me-3 mb-2">
-                <img src="<?= URL_SITE ?>/public/img/user/icons/<?= $_SESSION['user']->icone ?>" alt="Icone" class="user-icone rounded-circle me-2">
-                Olá, <?= explode(' ', trim($user->nome))[0] ?>
-            </p>
+        <div class="text-muted border-bottom pb-3">
+            <img src="<?= URL_SITE ?>/public/img/user/icons/<?= $_SESSION['user']->icone ?>" alt="Icone" class="user-icone rounded-circle me-2">
+            Olá, <?= explode(' ', trim($user->nome))[0] ?>
+
+            <div class="float-end d-flex">
+                <p class="p-0">Seus feitos |</p>
+                <p class="mx-2 p-0"><i class="bi-clipboard2-data-fill"></i> O.S - <b><?= count($servicos) ?></b></p>
+                <p class="mx-2 p-0"><i class="bi-handbag-fill"></i> Vendas - <b><?= count($servicos) ?></b></p>
+            </div>
         </div>
     </div>
 
@@ -17,7 +21,7 @@
 
         <div class="col-6 col-md-4 col-lg-3">
             <a href='/usuarios' class="card bg-dark text-light text-decoration-none">
-                <div class="card-body py-3">
+                <div class="card-body py-3 hover-border">
                     <div class='d-flex'>
                         <h3><i class="bi-person-lines-fill"></i></h3>
                         <p class="my-auto ms-2">Usuários</p>
@@ -39,7 +43,7 @@
 
         <div class="col-6 col-md-4 col-lg-3">
             <a href='/clientes' class="card bg-dark text-light text-decoration-none">
-                <div class="card-body py-3">
+                <div class="card-body py-3 hover-border">
                     <div class='d-flex'>
                         <h3><i class="bi-people-fill"></i></h3>
                         <p class="my-auto ms-2">Clientes</p>
@@ -61,19 +65,19 @@
 
         <div class="col-6 col-md-4 col-lg-3">
             <a href='#' class="card bg-dark text-light text-decoration-none">
-                <div class="card-body py-3">
+                <div class="card-body py-3 hover-border">
                     <div class='d-flex'>
-                        <h3><i class="bi-box-seam-fill"></i></h3>
-                        <p class="my-auto ms-2">Produtos</p>
+                        <h3><i class="bi-clipboard2-data-fill"></i></h3>
+                        <p class="my-auto ms-2">O.S</p>
                     </div>
                     <?php
-                        if(isset($produtos) && count($produtos) > 0){
+                        if(isset($os) && count($os) > 0){
                     ?>
-                        <h3 class="my-2"><?= count($produtos) ?> </h3>
+                        <h3 class="my-2"><?= count($os) ?> </h3>
                     <?php
                         }else{
                     ?>
-                        <p class="my-2">Ainda não há produtos</p>
+                        <p class="my-2">Ainda não há O.S</p>
                     <?php
                         }
                     ?>
@@ -83,7 +87,7 @@
 
         <div class="col-6 col-md-4 col-lg-3">
             <a href='#' class="card bg-dark text-light text-decoration-none">
-                <div class="card-body py-3">
+                <div class="card-body py-3 hover-border">
                     <div class='d-flex'>
                         <h3><i class="bi-handbag-fill"></i></h3>
                         <p class="my-auto ms-2">Vendas</p>
@@ -105,7 +109,7 @@
 
         <div class="col-6 col-md-4 col-lg-3">
             <a href='/servicos' class="card bg-dark text-light text-decoration-none">
-                <div class="card-body py-3">
+                <div class="card-body py-3 hover-border">
                     <div class='d-flex'>
                         <h3><i class="bi-tools"></i></h3>
                         <p class="my-auto ms-2">Serviços</p>
@@ -118,6 +122,28 @@
                         }else{
                     ?>
                         <p class="my-2">Ainda não há serviços</p>
+                    <?php
+                        }
+                    ?>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-md-4 col-lg-3">
+            <a href='#' class="card bg-dark text-light text-decoration-none">
+                <div class="card-body py-3 hover-border">
+                    <div class='d-flex'>
+                        <h3><i class="bi-box-seam-fill"></i></h3>
+                        <p class="my-auto ms-2">Produtos</p>
+                    </div>
+                    <?php
+                        if(isset($produtos) && count($produtos) > 0){
+                    ?>
+                        <h3 class="my-2"><?= count($produtos) ?> </h3>
+                    <?php
+                        }else{
+                    ?>
+                        <p class="my-2">Ainda não há produtos</p>
                     <?php
                         }
                     ?>
