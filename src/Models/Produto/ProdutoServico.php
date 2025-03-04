42ccbde87de16af5c9e1d8cd985d52bfea3ece07
andreeezinho <andreeesapucaia@gmail.com>
2025-03-04 15:48:08 -0300

    chore create products_services branch

---
+++ src/Models/Produto/ProdutoServico.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Produto;
+
+use App\Models\Traits\Uuid;
+
+class ProdutoServico {
+
+    use Uuid;
+
+    public $id;
+    public $uuid;
+    public $produtos_id;
+    public $servicos_id;
+    public $created_at;
+    public $updated_at;
+
+    public function create() : ProdutoServico {
+        $produto_servico = new ProdutoServico;
+        $produto_servico->id = $data['id'] ?? null;
+        $produto_servico->uuid = $data['uuid'] ?? $this->generateUUID();
+        $produto_servico->produtos_id = $data['produtos_id'] ?? null;
+        $produto_servico->servicos_id = $data['servicos_id'] ?? null;
+        $produto_servico->created_at = $data['created_at'] ?? null;
+        $produto_servico->updated_at = $data['updated_at'] ?? null;
+        return $produto_servico;
+    }
+    
+}
